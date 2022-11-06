662edd9cb8fe71b87b18b52c86cf8abfe4c2821c
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2022-11-06 21:17:28 +0100

    creation de l'interface Command, classe DnsTUI et RechercheIP cette dernière implemente Command

---
+++ src/main/java/fr/uvsq/cprog/collex/DnsTUI.java
@@ -0,0 +1,14 @@
+package fr.uvsq.cprog.collex;
+
+public class DnsTUI {
+    private Command command ;
+    private Dns dns;
+    private DnsTUI(Dns dns){
+        this.dns  = dns ;
+
+    }
+    public Command nextCommand(String cmd){
+
+    }
+
+}
---
+++ src/main/java/fr/uvsq/cprog/collex/RecherchIP.java
@@ -0,0 +1,35 @@
+package fr.uvsq.cprog.collex;
+
+public class RecherchIP implements Command{
+    private Dns dns ;
+    private AdresseIP adresseIP;
+    public RecherchIP(Dns dns, String cmd){
+        if(dns == null){
+            throw new NullPointerException("pas du dns");
+        }
+        this.dns = dns;
+        this.adresseIP = new AdresseIP(cmd);
+
+    }
+
+
+
+    @Override
+    public String execute() {
+        try {
+            DnsItem dnsItem = dns.getItem(this.adresseIP);
+            if (dnsItem == null) {
+                return  "non trouvé";
+            }
+            else {
+                return dnsItem.getNomMachine().toString();
+            }
+
+        }catch (Exception e) {
+            return "Une erreur est survenue";
+        }
+    }
+
+
+
+}
