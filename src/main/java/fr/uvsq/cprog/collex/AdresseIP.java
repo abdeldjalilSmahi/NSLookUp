467e64a06aac95196944f064a1d36f0af955f6d8
uvsq22205250 <abdeldjalil.smahi@ens.uvsq.fr>
2022-11-13 14:54:16 +0100

    modification du style en respectant google checkstyle dans la classe AdresseIP

--- src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
+++ src/main/java/fr/uvsq/cprog/collex/AdresseIP.java
@@ -1,33 +1,43 @@
 package fr.uvsq.cprog.collex;
+/**
+ * importer la méthode de vérification de la validation d'une adresse ip
+ */
 
 import org.apache.commons.validator.routines.InetAddressValidator;
 
-
+/**
+ * class {@link AdresseIP} implemente l'interface {@link IValidator} prend comme parametre un {@link String} et on valid si c'est une adresseIP en utilisant pour cela une dependency de commons.validator
+ */
 public class AdresseIP implements IValidator {
 
   private String adresseIP;
 
+  /**
+   * @param adresseIP
+   */
   public AdresseIP(String adresseIP) {
     validate(adresseIP);
     this.adresseIP = adresseIP;
 
   }
 
+  /**
+   * @return un String : adresseIP
+   */
   public String getAdresseIP() {
     return adresseIP;
   }
 
-  /*
-  On importe une méthode de l'api commons-validator qui permet de valider les adresses ip
+  /**
+   * On importe une méthode de l'api commons-validator qui permet de valider les adresses ip
    */
   public static boolean isValidIpAddress(String ipAddress) {
     return InetAddressValidator.getInstance().isValidInet4Address(ipAddress);
   }
 
-    /*
-    validez si l'utilsateur essaie d'instancier une valide addresse ip
-     */
-
+  /**
+   * validez si l'utilsateur essaie d'instancier une valide addresse ip
+   */
   @Override
   public void validate(String adrIp) {
     if (!(isValidIpAddress(adrIp))) {
@@ -35,9 +45,9 @@ public class AdresseIP implements IValidator {
     }
   }
 
-    /*
-    On redefinit la méthode equals pour simplifier les comparaisons entre adresses ip après .
-     */
+  /**
+   * On redefinit la méthode equals pour simplifier les comparaisons entre adresses ip après .
+   */
 
   @Override
   public boolean equals(Object obj) {
@@ -55,8 +65,8 @@ public class AdresseIP implements IValidator {
     return this.adresseIP.equals(strAdresseIP1);
   }
 
-  /*
-  redefinir la methode toString
+  /**
+   * redefinir la methode toString
    */
   @Override
   public String toString() {
